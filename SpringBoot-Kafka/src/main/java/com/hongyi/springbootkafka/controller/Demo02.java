a1218efbc7040c51568cb9e3366ee29bd8fd7182
LINLvHua <the_hongyi@163.com>
2023-02-18 09:22:40 +0800

    leetcode:回文数

--- SpringBoot-Kafka/src/main/java/com/hongyi/springbootkafka/controller/Demo02.java
+++ SpringBoot-Kafka/src/main/java/com/hongyi/springbootkafka/controller/Demo02.java
@@ -9,7 +9,41 @@ package com.hongyi.springbootkafka.controller;
 public class Demo02 {
     public static void main(String[] args) {
         String str = "babad";
+        String s = longestPalindrome(str);
+        System.out.println("s = " + s);
+    }
 
+    public static String longestPalindrome(String s) {
+        String max = "";
+        int len = s.length();
 
+        for (int i = 0; i < len; i++) {
+            for (int j = i; j < len; j++) {
+                // 取出来
+                String split = s.substring(i, j+1);
+                if (isPalindromic(split)){ // 如果是最大的,那么重置数据
+                    max = split.length()>max.length()?split:max;
+                }
+            }
+        }
+
+        return max;
+    }
+
+    /**
+     * 给定字段是否为回文数
+     * @param split
+     * @return
+     */
+    private static boolean isPalindromic(String split) {
+        int len = split.length();
+        // 字节一个一个的进行对比;
+        for (int i = 0; i <len/2; i++) {
+            if (split.charAt(i) != split.charAt(len-i-1)){
+                return false;
+            }
+
+        }
+        return true;
     }
 }
